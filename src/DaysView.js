626377c1070cf9b835b8c09fbbf43a6ad6f7e79b
Dinesh Kumar <dinesh@rocketsource.co>
2023-07-28 11:04:32 +0530

    Fixed error

--- src/DaysView.js
+++ src/DaysView.js
@@ -16,11 +16,11 @@ var DateTimePickerDays = createClass({
 		tableChildren = [
 			React.createElement('thead', { key: 'th' }, [
 				React.createElement('tr', { key: 'h' }, [
-					React.createElement('th', { key: 'p', className: 'rdtYearPrev', onClick: this.props.subtractTime( 1, 'years' )}, React.createElement('span', {}, '‹‹' )),
+					React.createElement('th', { key: 'py', className: 'rdtYearPrev', onClick: this.props.subtractTime( 1, 'years' )}, React.createElement('span', {}, '‹‹' )),
 					React.createElement('th', { key: 'p', className: 'rdtPrev', onClick: this.props.subtractTime( 1, 'months' )}, React.createElement('span', {}, '‹' )),
 					React.createElement('th', { key: 's', className: 'rdtSwitch', onClick: this.props.showView( 'months' ), colSpan: 3, 'data-value': this.props.viewDate.month() }, locale.months( date ) + ' ' + date.year() ),
 					React.createElement('th', { key: 'n', className: 'rdtNext', onClick: this.props.addTime( 1, 'months' )}, React.createElement('span', {}, '›' )),
-					React.createElement('th', { key: 'n', className: 'rdtYearNext', onClick: this.props.addTime( 1, 'years' )}, React.createElement('span', {}, '››' ))
+					React.createElement('th', { key: 'ny', className: 'rdtYearNext', onClick: this.props.addTime( 1, 'years' )}, React.createElement('span', {}, '››' ))
 				]),
 				React.createElement('tr', { key: 'd'}, this.getDaysOfWeek( locale ).map( function( day, index ) { return React.createElement('th', { key: day + index, className: 'dow'}, day ); }) )
 			]),
